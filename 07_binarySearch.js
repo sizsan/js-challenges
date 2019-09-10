f2f3f6782bac15730cea3b35fe1339cc034e3433
sizsan <sizsan2001@gmail.com>
2019-09-11 09:22:47 +1000

    binary search complete

--- 07_binarySearch.js
+++ 07_binarySearch.js
@@ -1,10 +1,10 @@
 /*
 Write a method which will act as a binary search which will find the 
 position and the actual number of steps required to find the position. 
-When the array has an even number of values the midpoint index will be rounded up.
+When the binArr has an even number of values the midpoint index will be rounded up.
 
 Example:
-    binaryArray = [1,5,8,12,20,21,35]
+    binarybinArr = [1,5,8,12,20,21,35]
     searchValue = 8
 
     In this case the index returned would be 2 and it should take 3 steps. 
@@ -13,23 +13,29 @@ Example:
     In the third and final step we are only left with 8 at index 2.
 
 */
-
-function binarySearch(binArr, searchValue) {
-    let steps = 0 
-    let arr = [...binArr]
-    while (arr.length > 1){
-        steps ++
-    let mid = arr.length / 2
-        if (arr[mid] < searchValue){
-            arr.splice(mid, arr.length)
+    function binarySearch(binArr, searchValue){
+        let startIndex = 0;
+        let endIndex = binArr.length - 1;
+        let steps = 0;
+        while(startIndex <= endIndex) {
+          let middleIndex = Math.floor((startIndex + endIndex) / 2);
+          steps += 1 
+          if(searchValue === binArr[middleIndex]) {
+            return [middleIndex, steps]
+          }
+          else if(searchValue > binArr[middleIndex]) {
+            startIndex = middleIndex + 1;
+          }
+          else if(searchValue < binArr[middleIndex]) {
+             endIndex = middleIndex - 1;
+          }
+          else {
+            console.log("Not Found this loop iteration. Looping another iteration.")
+          }
         }
-        else if (arr[mid] > searchValue){
-            arr.splice(0, mid)
-        }
-    }
-    return steps
-    return arr
-}
+        
+        console.log("searchValue value not found in binArr");
+      }
 
 
 module.exports = {
