a3eea6e4c9c3b78acff7f9122678776649ca35be
sizsan <sizsan2001@gmail.com>
2019-09-16 09:55:19 +1000

    09 complete

--- 09_oddOrEven.js
+++ 09_oddOrEven.js
@@ -9,7 +9,7 @@ If the input array is empty consider it as: [0] (array with a zero).
 */
 
 function oddOrEven(array) {
-    //enter code here
+   return (array.reduce((a, b) => a + b, 0) % 2 == 0) ? 'even' : 'odd'
 }
 
 module.exports = {oddOrEven};
